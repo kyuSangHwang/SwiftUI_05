859cb280989b351e9d9b6f62e6c9787fdf09885c
DopeKS <kyusang.dev@gmail.com>
2024-04-23 16:42:02 +0900

    Initialize Project UsingSFSymbols

---
+++ UsingSFSymbols/UsingSFSymbols/UsingSFSymbolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsingSFSymbolsApp.swift
+//  UsingSFSymbols
+//
+//  Created by 황규상 on 4/23/24.
+//
+
+import SwiftUI
+
+@main
+struct UsingSFSymbolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
