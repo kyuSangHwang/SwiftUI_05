d3f90f291b09199b4007d796f5592fae8652e88b
DopeKS <kyusang.dev@gmail.com>
2024-04-23 14:29:13 +0900

    Add files and NavigationLinks

--- Buttons/Buttons/ContentView.swift
+++ Buttons/Buttons/ContentView.swift
@@ -9,13 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack(spacing: 44) {
+                NavigationLink("Buttons") {
+                    ButtonView()
+                }
+                NavigationLink("EditButtons") {
+                    EditButtonView()
+                }
+                NavigationLink("MenuButtons") {
+                    MenuButtonView()
+                }
+                NavigationLink("PasteButtons") {
+                    PasteButtonView()
+                }
+                NavigationLink("Details about text") {
+                    Text("Very long text that should not be deplayed in a single line because it is not good design")
+                        .padding()
+                        .navigationTitle(Text("Detail"))
+                }
+            }
+            .navigationTitle(Text("Main View"))
         }
-        .padding()
     }
 }
 
---
+++ Buttons/Buttons/MenuButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  MenuButtonView.swift
+//  Buttons
+//
+//  Created by 황규상 on 4/23/24.
+//
+
+import SwiftUI
+
+struct MenuButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MenuButtonView()
+}
