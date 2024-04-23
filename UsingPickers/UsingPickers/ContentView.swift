f027f3aa077b7e3908cbf5e1c8ab0202b5da8eb7
DopeKS <kyusang.dev@gmail.com>
2024-04-23 15:47:35 +0900

    Pickers

--- UsingPickers/UsingPickers/ContentView.swift
+++ UsingPickers/UsingPickers/ContentView.swift
@@ -8,14 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var choice = 0
+    @State private var showText = false
+    @State private var transitModes = ["Bike", "Car", "Bus"]
+    @State private var sliderVal: Float = 0
+    @State private var gameTime = Date()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Form {
+            Section {
+                Picker("Transit Modes", selection: $choice) {
+                    ForEach(0..<transitModes.count, id: \.self) { index in
+                        Text(transitModes[index])
+                    }
+                }
+                .pickerStyle(.segmented)
+                
+                Text("Current choice: \(transitModes[choice])")
+            }
+            
+            Section {
+                Toggle(isOn: $showText) {
+                    Text("Show Text")
+                }
+                
+                if showText {
+                    Text("The Text toggle is on")
+                }
+            }
+            
+            Section {
+                Slider(value: $sliderVal, in: 0...10, step: 0.001)
+                Text("Slider current value: \(sliderVal, specifier: "%.3f")")
+            }
         }
-        .padding()
     }
 }
 
