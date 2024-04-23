cfaffdee92bf5cceb183a01be3e7f5997702c1a5
DopeKS <kyusang.dev@gmail.com>
2024-04-23 14:44:49 +0900

    ButtonView, EditButtonView

--- Buttons/Buttons/ButtonView.swift
+++ Buttons/Buttons/ButtonView.swift
@@ -8,11 +8,49 @@
 import SwiftUI
 
 struct ButtonView: View {
+    @State var count: Int = 0
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Welcome to your second view")
+            Text("Current count value: \(count)")
+            Button {
+                count += 1
+            } label: {
+                Text("Tap to Increment count")
+                    .fontWeight(.bold)
+                    .foregroundStyle(.white)
+                    .padding()
+                    .background(.blue)
+                    .clipShape(Capsule())
+            }
+            Button {
+                count -= 1
+            } label: {
+                Text("Tap to decrement count")
+                    .fontWeight(.bold)
+                    .foregroundStyle(.white)
+                    .padding()
+                    .background(.blue)
+                    .clipShape(Capsule())
+            }
+            Button {
+                count = 0
+            } label: {
+                Text("Tap to reset count")
+                    .fontWeight(.bold)
+                    .foregroundStyle(.white)
+                    .padding()
+                    .background(.blue)
+                    .clipShape(Capsule())
+            }
+        }
+        .navigationTitle("Button View")
     }
 }
 
 #Preview {
-    ButtonView()
+    NavigationStack {
+        ButtonView()
+    }
 }
