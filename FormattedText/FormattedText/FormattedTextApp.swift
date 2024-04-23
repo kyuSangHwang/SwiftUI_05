ab62800e9a4a4fb1e9fff7e88d1b22c5da01d127
DopeKS <kyusang.dev@gmail.com>
2024-04-23 13:34:41 +0900

    frame modifier

---
+++ FormattedText/FormattedText/FormattedTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormattedTextApp.swift
+//  FormattedText
+//
+//  Created by 황규상 on 4/23/24.
+//
+
+import SwiftUI
+
+@main
+struct FormattedTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
