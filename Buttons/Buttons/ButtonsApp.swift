a0716e1b495d3098b258862aff1141241e74c4cd
DopeKS <kyusang.dev@gmail.com>
2024-04-23 14:17:19 +0900

    Initialize Buttons Project

---
+++ Buttons/Buttons/ButtonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonsApp.swift
+//  Buttons
+//
+//  Created by 황규상 on 4/23/24.
+//
+
+import SwiftUI
+
+@main
+struct ButtonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
