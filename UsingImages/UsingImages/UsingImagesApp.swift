b3bec4b521fbba17b93c2d5905f9fed8d7af66f7
DopeKS <kyusang.dev@gmail.com>
2024-04-23 14:05:03 +0900

    Initialize Project UsingImage with image

---
+++ UsingImages/UsingImages/UsingImagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsingImagesApp.swift
+//  UsingImages
+//
+//  Created by 황규상 on 4/23/24.
+//
+
+import SwiftUI
+
+@main
+struct UsingImagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
