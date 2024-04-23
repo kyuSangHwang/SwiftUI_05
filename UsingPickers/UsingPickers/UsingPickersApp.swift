d839f0c9d664c083af4fb5615f006281b7aacba3
DopeKS <kyusang.dev@gmail.com>
2024-04-23 15:39:35 +0900

    Initialize Project UsingPickers

---
+++ UsingPickers/UsingPickers/UsingPickersApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsingPickersApp.swift
+//  UsingPickers
+//
+//  Created by 황규상 on 4/23/24.
+//
+
+import SwiftUI
+
+@main
+struct UsingPickersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
