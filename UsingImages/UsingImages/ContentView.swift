9dfd641c802b7eb0e8cca2adb37cf0b66efc95c0
DopeKS <kyusang.dev@gmail.com>
2024-04-23 14:13:21 +0900

    Using Images

--- UsingImages/UsingImages/ContentView.swift
+++ UsingImages/UsingImages/ContentView.swift
@@ -10,10 +10,23 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image("dogs1")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            
+            Image(.dogAndNature)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 300, height: 200)
+                .clipShape(Circle())
+                .overlay(Circle().stroke(.blue, lineWidth: 6))
+                .shadow(radius: 10)
+                .padding()
+            
+            Image(uiImage: UIImage(resource: .dog2))
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 200, height: 200)
         }
         .padding()
     }
