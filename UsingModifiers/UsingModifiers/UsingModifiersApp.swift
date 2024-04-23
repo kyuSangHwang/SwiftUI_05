2e5d5a5b96a4c98475c55a1f295992903c32bbbe
DopeKS <kyusang.dev@gmail.com>
2024-04-23 16:03:24 +0900

    Initialize Project UsingModifiers

---
+++ UsingModifiers/UsingModifiers/UsingModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsingModifiersApp.swift
+//  UsingModifiers
+//
+//  Created by 황규상 on 4/23/24.
+//
+
+import SwiftUI
+
+@main
+struct UsingModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
