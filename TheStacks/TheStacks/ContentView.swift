a529e1f10be389c7c7505d304fb24ba876cc2958
DopeKS <kyusang.dev@gmail.com>
2024-04-23 13:19:50 +0900

    The Stacks

--- TheStacks/TheStacks/ContentView.swift
+++ TheStacks/TheStacks/ContentView.swift
@@ -10,12 +10,36 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("VStack Item 1")
+            Text("VStack Item 2")
+            Spacer()
+            Divider()
+                .background(.black)
+            Text("VStack Item 3")
+            HStack {
+                Text("HStack Item 1")
+                Divider()
+                    .background(.black)
+                Spacer()
+                Text("HStack Item 2")
+                Divider()
+                    .background(.black)
+                Spacer()
+                Text("HStack Item 2")
+            }
+            .background(.red)
+            ZStack {
+                Text("ZStack Item 1")
+                    .padding()
+                    .background(.green)
+                    .opacity(0.8)
+                Text("ZStack Item 2")
+                    .padding()
+                    .background(.green)
+                    .offset(x: 80, y: -400) // 상대좌표
+            }
         }
-        .padding()
+        .background(.blue)
     }
 }
 
