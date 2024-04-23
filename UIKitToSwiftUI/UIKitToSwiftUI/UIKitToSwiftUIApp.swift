01ebd04039c1cbff636e5e04e74779cfaa421ed7
DopeKS <kyusang.dev@gmail.com>
2024-04-23 17:25:02 +0900

    Initialize Project UIKitToSwiftUI

---
+++ UIKitToSwiftUI/UIKitToSwiftUI/UIKitToSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIKitToSwiftUIApp.swift
+//  UIKitToSwiftUI
+//
+//  Created by 황규상 on 4/23/24.
+//
+
+import SwiftUI
+
+@main
+struct UIKitToSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
