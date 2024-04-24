a0bc54c15538a50797d38a9ae1f0b1f91de5be37
DopeKS <kyusang.dev@gmail.com>
2024-04-24 10:48:36 +0900

    Data seperate

---
+++ StaticList/StaticList/WeatherRow.swift
@@ -0,0 +1,28 @@
+//
+//  WeatherRow.swift
+//  StaticList
+//
+//  Created by 황규상 on 4/24/24.
+//
+
+import SwiftUI
+
+struct WeatherRow: View {
+    var weather: WeatherInfo
+    
+    var body: some View {
+        HStack {
+            Image(systemName: weather.image)
+                .frame(width: 50, alignment: .leading)
+            Text("\(weather.temp)°F")
+                .frame(width: 80, alignment: .leading)
+            Text(weather.city)
+        }
+        .font(.system(size: 25))
+        .padding()
+    }
+}
+
+#Preview {
+    WeatherRow(weather: WeatherInfo(image: "snow", temp: 5, city: "New York"))
+}
