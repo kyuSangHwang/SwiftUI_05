24c2b078a0292d13183a283d6177ee0413665e2e
DopeKS <kyusang.dev@gmail.com>
2024-04-24 11:02:47 +0900

    Initialize Project ListActions

---
+++ ListActions/ListActions/ListActionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListActionsApp.swift
+//  ListActions
+//
+//  Created by 황규상 on 4/24/24.
+//
+
+import SwiftUI
+
+@main
+struct ListActionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
