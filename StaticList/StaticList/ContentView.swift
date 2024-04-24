8fc7c3687e07192061dace9de0428790edde126b
DopeKS <kyusang.dev@gmail.com>
2024-04-24 10:28:31 +0900

    Initialize Project StaticList

---
+++ StaticList/StaticList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StaticList
+//
+//  Created by 황규상 on 4/24/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StaticList/StaticList/StaticListApp.swift
@@ -0,0 +1,17 @@
+//
+//  StaticListApp.swift
+//  StaticList
+//
+//  Created by 황규상 on 4/24/24.
+//
+
+import SwiftUI
+
+@main
+struct StaticListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
