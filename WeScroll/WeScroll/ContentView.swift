0eede67c1374de749b5d081e5817e89b4baeca67
DopeKS <kyusang.dev@gmail.com>
2024-04-24 10:14:48 +0900

    ScrollView and Indicators

--- WeScroll/WeScroll/ContentView.swift
+++ WeScroll/WeScroll/ContentView.swift
@@ -8,14 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    let letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p"]
+    
+    @State private var flashIndicators = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            ScrollView {
+                ForEach(letters, id: \.self) { letter in
+                    Image(systemName: letter)
+                        .font(.largeTitle)
+                        .foregroundStyle(.yellow)
+                        .frame(width: 50, height: 50)
+                        .background(.blue)
+                        .symbolVariant(.circle.fill)
+                }
+            }
+            .frame(width:50, height:200)
+            
+            ScrollView(.horizontal) {
+                HStack{
+                    ForEach(letters, id: \.self){ name in
+                        Image(systemName: name)
+                            .font(.largeTitle)
+                            .foregroundStyle(.yellow)
+                            .frame(width: 50, height: 50)
+                            .background(.blue)
+                            .symbolVariant(.circle.fill)
+                    }
+                }
+            }
+            .scrollIndicatorsFlash(trigger: flashIndicators)
+            .padding(.bottom)
+            
+            Button("Flash Indicators") {
+                flashIndicators.toggle()
+            }
         }
-        .padding()
+        .scrollIndicators(.hidden, axes: .vertical)
     }
 }
 
