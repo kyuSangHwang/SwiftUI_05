77c4c4249dcdb4dfa0adcd8240265e88ebe05c6b
DopeKS <kyusang.dev@gmail.com>
2024-04-24 11:48:46 +0900

    EditableList

--- EditableList/EditableList/ContentView.swift
+++ EditableList/EditableList/ContentView.swift
@@ -7,15 +7,27 @@
 
 import SwiftUI
 
+struct TodoItem: Identifiable {
+    let id = UUID()
+    var title: String
+}
+
 struct ContentView: View {
+    
+    @State private var todos = [
+        TodoItem(title: "Eat"),
+        TodoItem(title: "Sleep"),
+        TodoItem(title: "Code")
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        List($todos) { todo in
+            TextField("Todo Item", text: todo.title)
         }
-        .padding()
+        
+//        List(0..<todos.count) { index in
+//            TextField("Todo Item", text: $todos[index].title)
+//        }
     }
 }
 
