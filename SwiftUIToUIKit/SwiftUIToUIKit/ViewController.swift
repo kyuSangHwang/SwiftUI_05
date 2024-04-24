c0a65cbf0edd8fd0acc97cf3c016fffbed84b2b3
DopeKS <kyusang.dev@gmail.com>
2024-04-24 09:09:20 +0900

    Initialize Project SwiftUI to UIKit

---
+++ SwiftUIToUIKit/SwiftUIToUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftUIToUIKit
+//
+//  Created by 황규상 on 4/24/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
