5c64b4f17bdbfe648a8869f44eaadf7888fa2627
DopeKS <kyusang.dev@gmail.com>
2024-04-25 10:14:07 +0900

    Initialize Project ScrollViewReaders

---
+++ ScrollViewReaders/ScrollViewReaders/ScrollViewReadersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollViewReadersApp.swift
+//  ScrollViewReaders
+//
+//  Created by 황규상 on 4/25/24.
+//
+
+import SwiftUI
+
+@main
+struct ScrollViewReadersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
