b53d329568799e6c931cd4562bd0ffdd9aa4c6db
DopeKS <kyusang.dev@gmail.com>
2024-04-25 09:37:35 +0900

    LazyGrids

--- LazyGrids/LazyGrids/ContentView.swift
+++ LazyGrids/LazyGrids/ContentView.swift
@@ -8,12 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    let columnSpec = [
+        GridItem(.adaptive(minimum: 100))
+    ]
+    let rowSpec = [
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+    
+    let colors: [Color] = [.green, .red, .yellow, .blue]
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            ScrollView {
+                LazyVGrid(columns: columnSpec, spacing: 20) {
+                    ForEach(1...999, id: \.self) { index in
+                        Text("Item \(index)")
+                            .padding(EdgeInsets(top:20, leading: 15, bottom: 30, trailing: 15))
+                            .background(colors[index % colors.count])
+                            .clipShape(Capsule())
+                    }
+                }
+            }
+            
+            ScrollView {
+                LazyHGrid(rows: rowSpec, spacing: 20) {
+                    ForEach(1...999, id: \.self) { index in
+                        Text("Item \(index)")
+                            .foregroundStyle(.white)
+                            .padding(EdgeInsets(top:20, leading: 15, bottom: 30, trailing: 15))
+                            .background(colors[index % colors.count])
+                            .clipShape(Capsule())
+                    }
+                }
+            }
+            
         }
         .padding()
     }
