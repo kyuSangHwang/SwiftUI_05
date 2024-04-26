3b8cc53349b299beffc63212bcdae898ff1c0641
DopeKS <kyusang.dev@gmail.com>
2024-04-26 11:18:54 +0900

    Initialize Project NavigationStack Preview

---
+++ PreviewingInNavigationStack/PreviewingInNavigationStack/PreviewingInNavigationStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  PreviewingInNavigationStackApp.swift
+//  PreviewingInNavigationStack
+//
+//  Created by 황규상 on 4/26/24.
+//
+
+import SwiftUI
+
+@main
+struct PreviewingInNavigationStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
