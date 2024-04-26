945630e585a2d1b801e1398903a2e7053d70b4af
DopeKS <kyusang.dev@gmail.com>
2024-04-26 16:36:54 +0900

    Add SignUp UI

---
+++ SignUp/SignUp/SignUpView.swift
@@ -0,0 +1,67 @@
+//
+//  SignUpView.swift
+//  SignUp
+//
+//  Created by 황규상 on 4/26/24.
+//
+
+import SwiftUI
+
+struct Address {
+    var street: String = ""
+    var city: String = ""
+    var postalCode: String = ""
+}
+
+struct SignUpView: View {
+    @State private var firstName = ""
+    @State private var lastName = ""
+    @State private var address = Address()
+    @State private var address2 = Address()
+    @State private var lessThenTwo = false
+    @State private var userName = ""
+    @State private var password = ""
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                Section("Name") {
+                    TextField("First Name", text: $firstName)
+                    TextField("Last Name", text: $lastName)
+                }
+                
+                Section("Current Address") {
+                    TextField("Street Address", text: $address.street)
+                    TextField("City", text: $address.city)
+                    TextField("Postal Code", text: $address.postalCode)
+                    Toggle(isOn: $lessThenTwo) {
+                        Text("Have you lived here for 2+ years")
+                    }
+                }
+                
+                if !lessThenTwo {
+                    Section("Previous Address") {
+                        TextField("Street Address", text: $address2.street)
+                        TextField("City", text: $address2.city)
+                        TextField("Postal Code", text: $address2.postalCode)
+                    }
+                }
+                
+                Section {
+                    TextField("Create Username", text: $userName)
+                    SecureField("Create Password", text: $password)
+                }
+                
+                Button("Submit") {
+                    print("Form submit action here")
+                }
+            }
+            .navigationTitle("Sign Up")
+            .navigationBarTitleDisplayMode(.large)
+        }
+    }
+}
+
+#Preview {
+    SignUpView()
+}
