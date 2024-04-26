7eb38c563b354a17d7b061852a7f129c37cad325
DopeKS <kyusang.dev@gmail.com>
2024-04-26 16:23:54 +0900

    Initialize Project SignUp

---
+++ SignUp/SignUp/SignUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignUpApp.swift
+//  SignUp
+//
+//  Created by 황규상 on 4/26/24.
+//
+
+import SwiftUI
+
+@main
+struct SignUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
