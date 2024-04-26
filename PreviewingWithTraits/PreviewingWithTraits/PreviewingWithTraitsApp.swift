fd6267602b6b0b2267f4111e6105081aa8c1478b
DopeKS <kyusang.dev@gmail.com>
2024-04-26 11:33:09 +0900

    Initialize Project Traits Preview

---
+++ PreviewingWithTraits/PreviewingWithTraits/PreviewingWithTraitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PreviewingWithTraitsApp.swift
+//  PreviewingWithTraits
+//
+//  Created by 황규상 on 4/26/24.
+//
+
+import SwiftUI
+
+@main
+struct PreviewingWithTraitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
