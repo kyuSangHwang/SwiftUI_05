dcf62ca63c882f3e10b9ee9398ab1bd3966f07f2
DopeKS <kyusang.dev@gmail.com>
2024-04-29 11:33:09 +0900

    Add UI For RemoteConfig

--- RemoteConfig/RemoteConfig/ContentView.swift
+++ RemoteConfig/RemoteConfig/ContentView.swift
@@ -6,14 +6,37 @@
 //
 
 import SwiftUI
+import Firebase
+import FirebaseRemoteConfigSwift
+
+class RemoteConfiguration {
+    private var remoteConfig = Firebase.RemoteConfig.remoteConfig()
+}
 
 struct ContentView: View {
+    private var config = RemoteConfiguration()
+    @RemoteConfigProperty(key: "screenType", fallback: nil) var screenType: String?
+    
+    private var image: (name: String, color: Color) {
+        if screenType == "screenA" { // Tuple 형태의 Computed Property
+            ("a.square", .green)
+        } else if screenType == "screenB" {
+            ("b.square", .blue)
+        } else {
+            ("questionmark.square", .red)
+        }
+    }
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            if screenType != nil {
+                Image(systemName: image.name)
+                    .foregroundStyle(.red)
+                    .font(.system(size: 350))
+            } else {
+                ProgressView()
+                    .controlSize(.large)
+            }
         }
         .padding()
     }
