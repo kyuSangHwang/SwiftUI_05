79cf1d0f236c03b271a8547c3bcdc643028a5fbc
DopeKS <kyusang.dev@gmail.com>
2024-04-29 13:29:12 +0900

    Initialize Project FirebaseBasic

---
+++ FIrebaseBasic/FIrebaseBasic/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FIrebaseBasic
+//
+//  Created by 황규상 on 4/29/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FIrebaseBasic/FIrebaseBasic/FIrebaseBasicApp.swift
@@ -0,0 +1,17 @@
+//
+//  FIrebaseBasicApp.swift
+//  FIrebaseBasic
+//
+//  Created by 황규상 on 4/29/24.
+//
+
+import SwiftUI
+
+@main
+struct FIrebaseBasicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
